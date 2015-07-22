a8fdd9a325747ef6a67a54224fc40ffefed79dd4
Chris Lee <c_leeonidas41@yahoo.com>
2015-07-22 16:36:10 -0400

    created User Sign up/in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  root to: "users#index"
+  devise_for :users
+
+  # devise_scope :user do 
+  #   unauthenticated :user do
+  #     root :to => 'devise/sessions#new'
+  #   end
+
+  #   authenticated :user do
+  #     root :to => 'devise/sessions#new'
+  #   end
+  # end
+
+  root to: "users#show"
 end
