caa3b4a0e7520b7a03232a86c7a57f14a6ab87df
Chris Lee <c_leeonidas41@yahoo.com>
2015-07-23 17:12:03 -0400

    seeded db

--- Gemfile
+++ Gemfile
@@ -46,3 +46,5 @@ end
 
 gem 'bootstrap-sass'
 
+gem 'faker'
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+
+#Create Users
+15.times do
+  User.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: Faker::Lorem.characters(10)
+    )
+end
+
+
+users = User.all
+
+#Create Items
+100.times do
+  Item.create!(
+    user: users.sample,
+    name: Faker::Lorem.word
+  )
+end
+
+puts "Seed Finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
